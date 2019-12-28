6694ffcc3aacbd51b9a430bfa08cccd7654d1e68
Konrad Nojman <94konrad.n@gmail.com>
2019-12-28 16:32:06 +0100

    Refactor async functions to simpler form

--- src/utils/GithubApiUtils.js
+++ src/utils/GithubApiUtils.js
@@ -1,50 +1,44 @@
-export const findOrganization = input => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch(
-        `https://api.github.com/search/users?q=${input}+type:org`
-      );
-      const json = await response.json();
-      const { items: organizations } = json;
-      const bestMatch = organizations[0].login;
+export const findOrganization = async input => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/search/users?q=${input}+type:org`
+    );
+    const json = await response.json();
 
-      resolve(bestMatch);
-    } catch (error) {
-      reject(error);
-    }
-  });
+    if (json.totalCount === 0) return '';
+
+    const { items: organizations } = json;
+    const bestMatch = organizations[0].login;
+    return bestMatch;
+  } catch (error) {
+    console.error('errrr', error);
+  }
 };
 
-export const getOrganizationUsers = organizationName => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch(
-        `https://api.github.com/orgs/${organizationName}/members`
-      );
-      const json = await response.json();
+export const getOrganizationUsers = async organizationName => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/orgs/${organizationName}/members`
+    );
+    const json = await response.json();
 
-      const users = json.map(user => ({
-        id: user.id,
-        login: user.login
-      }));
+    const users = json.map(user => ({
+      id: user.id,
+      login: user.login
+    }));
 
-      resolve(users);
-    } catch (error) {
-      reject(error);
-    }
-  });
+    return users;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const getUserLastActivity = user => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch(
-        `https://api.github.com/users/${user}/events`
-      );
-      const json = await response.json();
-      resolve(json[0]?.type || 'User has no public activity');
-    } catch (error) {
-      reject(error);
-    }
-  });
+export const getUserLastActivity = async user => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${user}/events`);
+    const json = await response.json();
+    return json[0]?.type || 'User has no public activity';
+  } catch (error) {
+    console.error(error);
+  }
 };
