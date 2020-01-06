6b1e92f719d4d09536836c2d9f2e1a0bc78bcdfe
Konrad Nojman <94konrad.n@gmail.com>
2020-01-07 00:07:31 +0100

    Updates

---
+++ precache-manifest.83e43062244e3fdb71bbcaf35a7ad8c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0f6c0f0fdbddd307527cc768cbe956",
+    "url": "/don-gitto/index.html"
+  },
+  {
+    "revision": "2545aef265dff72e8b95",
+    "url": "/don-gitto/static/css/main.23fe7fc9.chunk.css"
+  },
+  {
+    "revision": "93a547bf119dfb938e53",
+    "url": "/don-gitto/static/js/2.0a275c1f.chunk.js"
+  },
+  {
+    "revision": "c5682c5d77e856e2d28652c66e6435c2",
+    "url": "/don-gitto/static/js/2.0a275c1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2545aef265dff72e8b95",
+    "url": "/don-gitto/static/js/main.61ba8cd7.chunk.js"
+  },
+  {
+    "revision": "759f92aaa96f7cb79663",
+    "url": "/don-gitto/static/js/runtime-main.ab5ba222.js"
+  }
+]);
